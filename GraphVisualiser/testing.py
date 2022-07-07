2e282df8cbab8ca874f02171b36d142e6548ba12
Vaibhav <vaibhavgoyalop@gmail.com>
2022-07-07 19:46:22 +0530

    Graph Visualiser

--- GraphVisualiser/testing.py
+++ GraphVisualiser/testing.py
@@ -12,34 +12,34 @@ def start(g, nodes, algo):
     will help me create the animation.
     """
 
+    dir = '/Users/vaibhavgoyal/Documents/Programming/Python/PycharmProjects/GraphVisualiserRecievesImages/static/output'
+    for f in os.listdir(dir):
+        os.remove(os.path.join(dir, f))
+
     # importing necessary modules
     print('Downloading started')
 
     # Defining the zip file URL
-    url = 'https://graphimages.herokuapp.com/solve'
-
-    # Split URL to get the file name
-    filename = url.split('/')[-1]
+    url = 'http://127.0.0.1:8000/solve'
 
     edges = ""
     for e in g:
         s = ' '.join([str(n) for n in e])
         edges += s + '\n'
-    print(edges)
-    print(nodes, algo)
+    print(edges, nodes, algo)
 
     payload = {"nodes": nodes, "algo": algo, "edges": edges}
 
     # Downloading the file by sending the request to the URL
     req = requests.get(url, params=payload)
     print('Downloading Completed')
-    print(req.content)
+
     # extracting the zip file contents
     zfile = zipfile.ZipFile(BytesIO(req.content))
-    zfile.extractall('static/output')
+    zfile.extractall('/Users/vaibhavgoyal/Documents/Programming/Python/PycharmProjects/GraphVisualiserRecievesImages/static/output')
+    return 5
 
-    # I have to return the number of images in the zip file
-    path = 'static/output'
-    images = len(os.listdir(path=path))
-    if (algo == 'dijkstra'): return images
-    return images
+
+g = [[1, 2, 1], [1, 3, 2], [2, 3, 4], [3, 4, 3], [4, 5, 2]]
+nodes = 5
+start(g, nodes, "dijkstra")
