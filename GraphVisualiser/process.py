c20f533e9b3617e48b7ddaf8e00af19c50c20ba2
Kanishk Garg <76126518+Kanishk-Garg4@users.noreply.github.com>
2022-08-11 00:05:53 +0530

    Update process.py

--- GraphVisualiser/process.py
+++ GraphVisualiser/process.py
@@ -10,22 +10,22 @@ def simulateGraph(request):
     2. message
     3. number of stages in the graph
     """
-    ALGOS = ["dfs", "bfs", "dijkstra", "kruskal", "prim"]
+    ALGOS = ["dfs", "bfs", "dijkstra", "kruskal", "prim"] 
     numberofnodes = request.GET.get('nodes', '')
     algo = request.GET.get('algo', '').lower()
     try:
-        numberofnodes = int(numberofnodes)
+        numberofnodes = int(numberofnodes) # checking if the user has not entered a string in the input
     except:
         return False, "Invalid number of nodes", -1
 
     if (numberofnodes == 0):
         return False, "Nodes can't be zero.", -1
 
-    if (algo not in ALGOS): return False, "Invalid Algo", -1
+    if (algo not in ALGOS): return False, "Invalid Algo", -1  # if the user has not entered the algos from the given placeholder
 
     givenEdgeString = request.GET.get('edges', '').strip()
     givenEdgeStrings = givenEdgeString.split("\r\n")
-    if (givenEdgeStrings[0] == ''): givenEdgeStrings = []
+    if (givenEdgeStrings[0] == ''): givenEdgeStrings = []  # storing the edges in the list
 
     g = [] # this will store edges
 
@@ -36,12 +36,12 @@ def simulateGraph(request):
         try:
             x = list(map(int, R.split()))
             if (len(x) == 2):
-                if (algo not in ["dfs", "bfs"]): return False, "Invalid Edges", -1
+                if (algo not in ["dfs", "bfs"]): return False, "Invalid Edges", -1  # checking if the length is 2 then the given algo can't be djikstra etc.
                 u, v = x
                 if (not u in nodes) or (not v in nodes): return False, "Invalid Edges", -1
                 g.append(x)
             elif (len(x) == 3):
-                if algo in ["dfs", "bfs"]: return False, "Invalid Edges", -1
+                if algo in ["dfs", "bfs"]: return False, "Invalid Edges", -1  # checking if the length is 3 then the given algo can't be dfs, bfs.
                 u, v, w = x
                 if (algo == "dijkstra") and w < 0: return False, "Dijkstra can't handle negative edge weights.", -1
                 if (not u in nodes) or (not v in nodes): return False, "Invalid Edges", -1
